18963696081da3fe823e2b42b214bff12ed99a33
deserthawk <ardadenli@gmail.com>
2022-04-17 14:01:05 +0300

    component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import qrCodePng from './images/image-qr-code.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+      <main>
+        <Component />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+function Component() {
+  return (
+    <div className='component'>
+      <img src={qrCodePng} alt='' />
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="https://www.frontendmentor.io/profile/deserthawk">deserthawk</a>.
     </div>
   );
 }
